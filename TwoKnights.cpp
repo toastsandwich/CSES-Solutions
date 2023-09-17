1656b2fd0c1f98f5f05c1c21f6633d2ad8660b17
Shreyas Mali <83450905+toastsandwich@users.noreply.github.com>
2023-09-17 11:20:49 +0530

    Create TwoKnights.cpp

---
+++ TwoKnights.cpp
@@ -0,0 +1,42 @@
+using namespace std;
+// using namespace boost::multiprecision;
+// using namespace boost::algorithm;
+ 
+// #include <boost/multiprecision/cpp_int.hpp>
+// #include <boost/algorithm/string.hpp>
+#include <bits/stdc++.h>
+#define      int         long long int
+//#define    bigInt      cpp_int
+#define      endl        "\n"
+#define      fast        ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define      all(x)      x.begin(),x.end()
+#define      NO          cout << "NO" << '\n'
+#define      YES         cout << "YES" << '\n'
+#define      CASE(i)     cout << "TESTCASE #" << i << endl
+template<typename T>
+void debug(T x) {
+    cerr << "debug :" << x << endl;
+}
+ 
+void solve() {
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n;i++) {
+        cout << (((i * i) * (i *i - 1)) / 2)- 4 * ((i - 1) * (i - 2)) << endl;
+    }
+}
+ 
+int32_t main() {
+    fast;
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    int t = 1;
+    // cin >> t;
+    // int p = 1;
+    while(t--) {
+        // CASE(p++);
+        solve();
+    }
+}
